81193f58723028e6ff868d2a95054e3fd8f8134d
NoopApp <thenoopapp@gmail.com>
2026-06-12 14:27:34 +0100

    noop 2.1.0

--- Casks/noop.rb
+++ Casks/noop.rb
@@ -1,6 +1,6 @@
 cask "noop" do
-  version "2.0"
-  sha256 "f1c9b613ed51c1acc51221deec4e90d62d33a122301fe14a3d3b2de36815063c"
+  version "2.1.0"
+  sha256 "b9f35e2b0f50aad7cc710bf9ab38579f8a8a4453bd8572f83639d0b5112eae70"
 
   url "https://github.com/NoopApp/noop/releases/download/v#{version}/NOOP-v#{version}-macos.zip"
   name "NOOP"
